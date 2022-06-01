8d04cfe05e14ef8326157222ef57393c62d7fab7
JoseMagIII <jose_maglaqueiii@dlsu.edu.ph>
2022-06-01 23:35:11 +0800

    Changed to threads

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+import threading
 
 # Function used to process threads
+import time
+
+
 def processthread(color, numberofthreads, initprocessid):
     # Process threads
-    for ctr in range(numberofthreads):
-        print(color + " #" + str(initprocessid+ctr))
+    if color == "Blue":
+        for ctr1 in range(numberofthreads):
+            blueThreads[ctr1+initprocessid].start()
+            blueThreads[ctr1 + initprocessid].join()
+            #lock.release()
+            #print(color + " #" + str(initprocessid+ctr1))
+
+    else:
+        for ctr1 in range(numberofthreads):
+            greenThreads[ctr1+initprocessid].start()
+            greenThreads[ctr1 + initprocessid].join()
+            #lock.release()
+            #print(color + " #" + str(initprocessid+ctr1))
 
 
+def dothread(color, processid):
+    lock.acquire()
+    print(color + " #" + str(processid))
+    lock.release()
 
 
 # Main function
@@ -16,6 +35,20 @@ if __name__ == '__main__':
     n = int(input("Number of slots inside fitting room: "))
     b = int(input("Number of blue threads "))
     g = int(input("Number of green threads "))
+
+
+    # Initialize threads
+    blueThreads = []
+    greenThreads = []
+    lock = threading.Lock()
+
+    for ctr in range(b):
+        blueThreads.append(threading.Thread(target=dothread, args=["Blue", ctr]))
+
+    for ctr in range(g):
+        greenThreads.append(threading.Thread(target=dothread, args=["Green", ctr]))
+
+
     print("\n")
     processedBlue = 0
     processedGreen = 0
@@ -82,5 +115,5 @@ if __name__ == '__main__':
                 print("Blue only")
 
     # Print final empty fitting room when done processing all threads
-    print("Empty fitting room\n")
+    print("Empty fitting room")
     #processthread("Blue", 3, 5)
