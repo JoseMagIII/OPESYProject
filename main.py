4502169b3ee0fc160b52205662776bd6acb1194d
JoseMagIII <jose_maglaqueiii@dlsu.edu.ph>
2022-06-11 20:04:01 +0800

    updated to semaphore

--- main.py
+++ main.py
@@ -3,28 +3,49 @@ import threading
 # Function used to process threads
 import time
 
+# Number of threads that has to be processed
+processCount = 0
+
+# Number of threads that has been processed per batch
+count = 0
 
 def processthread(color, numberofthreads, initprocessid):
+
+    global processCount
+    global count
+    processCount = numberofthreads
+
     # Process threads
     if color == "Blue":
         for ctr1 in range(numberofthreads):
             blueThreads[ctr1+initprocessid].start()
             blueThreads[ctr1 + initprocessid].join()
-            #lock.release()
-            #print(color + " #" + str(initprocessid+ctr1))
 
     else:
         for ctr1 in range(numberofthreads):
             greenThreads[ctr1+initprocessid].start()
             greenThreads[ctr1 + initprocessid].join()
-            #lock.release()
-            #print(color + " #" + str(initprocessid+ctr1))
 
+    count = 0
 
 def dothread(color, processid):
-    lock.acquire()
+    global count
+    global processCount
+
+    # When thread first enters room, print only allowed color
+    if count == 0:
+        print(color + " only")
+
+    rooms.acquire()
+
+    count = count + 1
     print(color + " #" + str(processid))
-    lock.release()
+
+    rooms.release()
+
+    # When final thread leaves fitting room, print empty fitting room
+    if count == processCount:
+        print("Empty fitting room\n")
 
 
 # Main function
@@ -40,7 +61,9 @@ if __name__ == '__main__':
     # Initialize threads
     blueThreads = []
     greenThreads = []
-    lock = threading.Lock()
+
+    # Limits the amount of threads that can enter the fitting room
+    rooms = threading.Semaphore(n)
 
     for ctr in range(b):
         blueThreads.append(threading.Thread(target=dothread, args=["Blue", ctr]))
@@ -56,10 +79,8 @@ if __name__ == '__main__':
     # Prioritize color with more amount of threads
     if b >= g:
         turn = True
-        print("Blue only")
     else:
         turn = False
-        print("Green only")
 
 
     # While there are still threads remaining to be processed
@@ -83,12 +104,7 @@ if __name__ == '__main__':
             # Pass the lock to green threads if there are still green threads remaining
             # This is to avoid starvation
             if(g > 0):
-
-                # Print empty fitting room. Will only switch once fitting room is empty.
-                print("Empty fitting room\n")
                 turn = False
-                # Print only allowed thread color
-                print("Green only")
 
         # This if and else statement ensures that threads being processed does not exceed the capacity of the fitting room
         else:
@@ -108,12 +124,5 @@ if __name__ == '__main__':
             # Pass the lock to blue threads if there are still blue threads remaining
             # This is to avoid starvation
             if (b > 0):
-                # Print empty fitting room. Will only switch once fitting room is empty.
-                print("Empty fitting room\n")
                 turn = True
-                # Print only allowed thread color
-                print("Blue only")
 
-    # Print final empty fitting room when done processing all threads
-    print("Empty fitting room")
-    #processthread("Blue", 3, 5)
